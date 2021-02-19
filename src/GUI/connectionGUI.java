63ae8b1a0ca600590f36d9db0bbb4d6dec6aa910
raphchar <50371278+raphchar@users.noreply.github.com>
2021-02-19 16:15:23 +0100

    Update connectionGUI.java
    Interface de connection avec JavaFX

--- src/GUI/connectionGUI.java
+++ src/GUI/connectionGUI.java
@@ -34,30 +34,26 @@ public class connectionGUI extends Application {
             this.primaryStage.setOnCloseRequest(event -> {
                 System.exit(0);}
                 );
-            IdentifiantConnection();
+            Connection();
         } catch (Exception e ) {e.printStackTrace();}
     }
 
-    public void IdentifiantConnection() throws Exception {
+    public void Connection() throws Exception {
         mainPane = new VBox();
-        mainPane.getStyleClass().add("IdentifiantConnection");
 
         //TextFields
         TextField nomUtilisateurTextField = new TextField();
-        nomUtilisateurTextField.getStyleClass().add("nomUtilisateurTextField");
         nomUtilisateurTextField.setPromptText("Nom d'utilisateur");
         nomUtilisateurTextField.setMinSize(250, 40);
+
         PasswordField motDePasseTextField = new PasswordField();
-        motDePasseTextField.getStyleClass().add("motDePasseTextField");
         motDePasseTextField.setPromptText("Mot de passe");
         motDePasseTextField.setMinSize(250, 40);
 
         //Error labels
         Label errorUsername = new Label("");
-        errorUsername.getStyleClass().add("labelError");
         errorUsername.setAlignment(Pos.CENTER_LEFT);
         Label errorPassword = new Label("");
-        errorPassword.getStyleClass().add("labelError");
         errorPassword.setAlignment(Pos.CENTER_LEFT);
 
         //TextField area
@@ -67,14 +63,17 @@ public class connectionGUI extends Application {
         areaText.setSpacing(10);
         areaText.setAlignment(Pos.BOTTOM_CENTER);
 
-        //--BUTTONS--
+        //Boutons
         Button bouttonConnection = new Button("Connection");
         bouttonConnection.setMinSize(150, 40);
-        bouttonConnection.getStyleClass().add("buttype1");
 
         Button bouttonNouveauCompte = new Button("Nouveau compte");
         bouttonNouveauCompte.setMinSize(150, 40);
-        bouttonNouveauCompte.getStyleClass().add("buttype2");
+        bouttonNouveauCompte.setOnAction(actionEvent -> {
+            try {
+                CreationCompte();
+            } catch (Exception e) {e.printStackTrace();}
+        });
 
         //Button Area
         VBox areaButton = new VBox();
@@ -101,7 +100,59 @@ public class connectionGUI extends Application {
         primaryStage.setScene(myScene);
         primaryStage.show();
     }
+    public void CreationCompte() throws Exception {
+        mainPane = new VBox();
 
+        // Textfields
+        TextField nomUtilisateurTextField = new TextField();
+        nomUtilisateurTextField.setPromptText("Entrez votre nom d'utilisateur");
+        nomUtilisateurTextField.setMinSize(250, 40);
+
+        PasswordField motDePasseTextField = new PasswordField();
+        motDePasseTextField.setPromptText("Votre mot de passe");
+        motDePasseTextField.setMinSize(250, 40);
+        PasswordField verificationMotdePasseTextField = new PasswordField();
+        verificationMotdePasseTextField.setPromptText("Vérification mot de passe");
+        verificationMotdePasseTextField.setMinSize(250, 40);
+
+        //Error Labels
+        Label errorNomUtilisateur = new Label("");
+        errorNomUtilisateur.setAlignment(Pos.CENTER_LEFT);
+        Label errorMotdePasse = new Label("");
+        errorMotdePasse.setAlignment(Pos.CENTER_LEFT);
+        Label errorMotdePasseVerifier = new Label("");
+        errorMotdePasseVerifier.setAlignment(Pos.CENTER_LEFT);
+
+        //TextField area
+        VBox areaText = new VBox();
+        areaText.getChildren().addAll(nomUtilisateurTextField,errorNomUtilisateur,motDePasseTextField,errorMotdePasse,verificationMotdePasseTextField,errorMotdePasseVerifier);
+        VBox.setMargin(areaText, new Insets(10,10,10,10));
+        areaText.setSpacing(5);
+        areaText.setAlignment(Pos.BOTTOM_CENTER);
+
+        //Buttons
+        Button bouttonCreation = new Button("Créer un nouveau compte");
+        Button bouttonConnection = new Button("Vous avez déjà un compte");
+        bouttonCreation.setMinSize(150,40);
+        bouttonConnection.setMinSize(150,40);
+        bouttonConnection.setOnAction(actionevent -> {
+            try {Connection();} catch (Exception e) {e.printStackTrace();}
+        });
+
+        VBox areaButton = new VBox();
+        areaButton.getChildren().addAll(bouttonCreation,bouttonConnection);
+        VBox.setMargin(areaButton, new Insets(10,10,10,10));
+        areaButton.setSpacing(30);
+        areaButton.setAlignment(Pos.CENTER);
+
+        //Creating main Pane
+        mainPane.setAlignment(Pos.CENTER);
+        mainPane.setSpacing(30);
+        mainPane.getChildren().addAll(areaText,areaButton);
+        Scene myScene = new Scene(mainPane, 300, 400);
+        primaryStage.setScene(myScene);
+        primaryStage.show();
+    }
     public static void main(String[] args) {
         launch(args);
     }
