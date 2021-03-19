8d06310d495fdb7a278c592647b0d1903bdc85be
raphchar <50371278+raphchar@users.noreply.github.com>
2021-03-19 09:31:49 +0100

    Update MainServeur.java

--- src/launchPattern/MainServeur.java
+++ src/launchPattern/MainServeur.java
@@ -5,8 +5,7 @@ import servPattern.ServeurTCP;
 public class MainServeur {
 
 	public static void main(String[] args) {
-		ServeurTCP myServ = new ServeurTCP(new UnContexte() , new Serveur() , 6666 );
+		ServeurTCP myServ = new ServeurTCP(new UnContexte() , new ProtocolePingPong() , 6666 );
 		myServ.start();
-		
 	}
 }
